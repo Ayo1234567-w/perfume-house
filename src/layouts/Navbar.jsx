05b10dd7d117702e0a0319e1301626e5453b6662
Haryomidey <fajembolaayo494@gmail.com>
2025-02-27 13:08:37 +0100

    first add main

---
+++ src/layouts/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { Outlet } from 'react-router-dom'
+import navLogo from "../assets/Group 9283.svg"
+import AuthBtn from '../components/AuthBtn'
+import menuImg from "../assets/Vector.svg"
+
+const Navbar = () => {
+    return (
+        <>
+            <nav className='container mx-auto p[25px] py-[33px] md:px-[99px] flex justify-between items-center'> 
+                {/* div-1 for nav logo */}
+                <div className='flex items-center gap-4'>
+                    <img src={navLogo} alt="nav-logo" />
+                    <h2 className='hidden md:block'>Perfume House</h2>
+                </div>
+                {/* div-2, for auth btns */}
+                <div className='hidden md:block'>
+                    <AuthBtn/>
+                </div>
+                {/* div for menu / hamburger img */}
+                <div className='md:hidden'>
+                    <img src={menuImg} alt="menu-img" />
+                </div>
+            </nav>
+        <Outlet/>
+        </>
+    )
+}
+
+export default Navbar
